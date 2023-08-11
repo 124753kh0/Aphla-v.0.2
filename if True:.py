f7a3b6123a621e8c42ddc860660e8a032b5c3c6d
JKGamin124 <105007877+124753kh0@users.noreply.github.com>
2023-08-10 19:25:16 -0500

    Add files via upload

---
+++ if True:.py
@@ -0,0 +1,25 @@
+if True:
+#Chareter Selection
+    e=input("Bob or Jerry {Bob} {IN PROGESS}")
+#Bob end
+    if e == ("bob"):
+        print("You Have Selected Bob!")
+        print("Bob Falls Into the Pit of Life Qustions...")
+    #is water wetQ-Bob
+        e1=input("Is Water Wet? [Yes] [No]")
+        if e1 == ("yes"):
+            print("no your wrong idoit{Idiot Ending}")
+        elif e1 == ("no"):
+            print("+========================================+")
+            print("|Water is not wet because wetness arises |")
+            print("|         from the interaction           |")
+            print("| between a liquid and a soild surface   |")
+            print("| in other words, wetness is a property  |")
+            print("|that occurs when water or another liquid|")
+            print("|        comes into contact with         |")
+            print("|            a solid object.             |")
+            print("+========================================+")
+#Jerry End
+    elif e == ("jerry"):
+        print("You Have Selected Jerry!")
+
